771378348582ccb0a2f5cdfd3bd42b9d9a1cb50d
Alex Battle <58659536+alex-battle@users.noreply.github.com>
2020-04-30 17:35:28 -0400

    generate users from api

--- script.js
+++ script.js
@@ -5,4 +5,26 @@ showMillibtn = document.getElementById('show-millionaires')
 sortbtn = document.getElementById('sort')
 calculateWealthbtn = document.getElementById('calculate-wealth')
 
+// put all the people in an array
 let data = []
+getRandomUser()
+getRandomUser()
+getRandomUser()
+// fetch random user and add money
+async function getRandomUser(){
+    const res = await fetch('https://randomuser.me/api')
+    const data = await res.json()
+    // console.log(data)
+    const user = data.results[0]
+    const newUser = {
+        name: `${user.name.first} ${user.name.last}`,
+        money: Math.floor(Math.random() * 1000000)
+    }
+    // console.log(newUser)
+    addData(newUser)
+}
+
+// add Data
+function addData(obj){
+    data.push(obj)
+}
