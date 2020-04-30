37e618a5d542a1b9449cf7a4098bcea5321984ca
Alex Battle <58659536+alex-battle@users.noreply.github.com>
2020-04-30 16:12:10 -0400

    html & css set

---
+++ script.js
@@ -0,0 +1,8 @@
+main = document.getElementById('main')
+addUserbtn = document.getElementById('add-user')
+doublebtn = document.getElementById('double')
+showMillibtn = document.getElementById('show-millionaires')
+sortbtn = document.getElementById('sort')
+calculateWealthbtn = document.getElementById('calculate-wealth')
+
+let data = []
